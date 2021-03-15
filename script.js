4121b69486466de91a05ee5f1b5537da669d2b12
Cosmin Gherghe <hello@cosmingherghe.dev>
2021-03-15 19:40:21 +0000

    testing querySelector

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+/*
+Testing: querySelector
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🏆 Corrent Number!';
+document.querySelector('.number').textContent = 10;
+document.querySelector('.score').textContent = 19;
+document.querySelector('.guess').value = 99;
+
+console.log(document.querySelector('.message').textContent);
+console.log(document.querySelector('.number').textContent);
+console.log(document.querySelector('.score').textContent);
+console.log(document.querySelector('.guess').value);
+*/
